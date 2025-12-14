2399894f2961153a94846f27a54ec2070147aaab
adela3tulen <pazderova.adela08@gmail.com>
2025-12-14 21:20:11 +0100

    o

--- praxe.js
+++ praxe.js
@@ -4,14 +4,3 @@ btn.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
 
- const links = document.querySelectorAll('#menu a');
-
-    links.forEach(link => {
-        link.addEventListener('click', () => {
-            // odeber active ze všech
-            links.forEach(l => l.classList.remove('active'));
-
-            // přidej active na kliknutý
-            link.classList.add('active');
-        });
-    });
